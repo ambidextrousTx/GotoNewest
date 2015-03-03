7d54a4ea1ab320390e801e7e7f1b70efe963003c
ambidextrousTx <ravisinha@my.unt.edu>
2015-03-03 05:47:50 -0600

    PyLint-based cleanup, add test case for no subdirectories

--- test/test_gn.py
+++ test/test_gn.py
@@ -1,12 +1,29 @@
+'''
+GotoNewest tests
+'''
+
 import unittest
 import sys
 sys.path.append('../src/')
 import gn
 
-class TestGotoNewest(unittest.TestCase):
+TEST_DIR = '/tmp/test'
 
-    def test_empty_base_dir_raises_error(self):
+class TestGotoNewest(unittest.TestCase):
+    ''' Test class for GotoNewest
+    '''
+
+    def test_empty_base_dir(self):
+        ''' If the base directory is empty, raise an
+        AttributeError
+        '''
         self.assertRaises(AttributeError, gn.transfer, None)
+
+    def test_base_dir_with_no_subdirs(self):
+        ''' If the base directory has no subdirectories,
+        raise an AttributeError
+        '''
+        self.assertRaises(AttributeError, gn.transfer, TEST_DIR)
 
 
 if __name__ == '__main__':
