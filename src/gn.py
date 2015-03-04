d39073a4528f3348efe98401cca3fa1190041577
ambidextrousTx <ravisinha@my.unt.edu>
2015-03-03 20:14:17 -0600

    Add handler for no subdirectories

--- src/gn.py
+++ src/gn.py
@@ -6,9 +6,14 @@ created in a base directory, provided the name of
 the base directory is supplied as an argument
 '''
 
+import os
+
 def transfer(basepath=None):
     ''' Transfer to the newest directory in the basepath
     directory
     '''
     if basepath is None:
+        raise AttributeError
+    subdirs = os.listdir(basepath)
+    if len(subdirs) == 0:
         raise AttributeError
